303478805613763b8bf316b63cb2bdcdbf44cc40
ARNAB <ItsABasak24>
2025-11-28 00:53:49 +0530

    Folder structure updated and bug fixed

--- api/app.py
+++ api/app.py
@@ -118,4 +118,5 @@ def debug_db():
         return {"error": str(e)}, 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
